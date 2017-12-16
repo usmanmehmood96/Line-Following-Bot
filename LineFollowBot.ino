5f6f1a84a9adfb4cd314c2d67f40f72b0013ced1
Usman Mehmood <160979@students.au.edu.pk>
2017-12-16 18:50:27 +0500

    Add files via upload

--- LineFollowBot.ino
+++ LineFollowBot.ino
@@ -1,22 +1,25 @@
+// Sensor LOW/HIGH constant
+const unsigned int hilo = 500; // For calibrating according to light levels. Use sketch 'analog IR'.
+
 // Motor A
-const int motorPin1   = 8;
-const int motorPin2   = 9;
-const int speedA      = 255; // For setting motor speed
+const unsigned int motorPin1   = 8;
+const unsigned int motorPin2   = 9;
+const unsigned int speedA      = 255; // For setting speed of motor A
 
 // Motor B
-const int motorPin3   = 10;
-const int motorPin4   = 11;
-const int speedB      = 255; // For setting motor speed
+const unsigned int motorPin3   = 10;
+const unsigned int motorPin4   = 11;
+const unsigned int speedB      = 255; // For setting speed of motor B
 
 // Sensors
-//const int farLeft     = A0;
-const int midLeft     = A1;
-const int nearLeft    = A2;
-const int centerLeft  = A3;
-const int centerRight = A4;
-const int nearRight   = A5;
-const int midRight    = A6;
-//const int farRight    = A7;
+const unsigned int farLeft     = A0;
+const unsigned int midLeft     = A1;
+const unsigned int nearLeft    = A2;
+//const unsigned int centerLeft  = A3;   not used
+//const unsigned int centerRight = A4;   not used
+const unsigned int nearRight   = A5;
+const unsigned int midRight    = A6;
+const unsigned int farRight    = A7;
 
 // Movement
 void moveForward()
@@ -31,7 +34,7 @@ void moveForward()
 void slightRight()
 {
   analogWrite(motorPin1, 0);
-  analogWrite(motorPin2, speedA-45);
+  analogWrite(motorPin2, speedA-55);
   analogWrite(motorPin3, speedB);
   analogWrite(motorPin4, 0);
 }
@@ -51,7 +54,7 @@ void moreRight()
 }
 void turnRight()
 {
-  analogWrite(motorPin1, speedA-65);
+  analogWrite(motorPin1, speedA);
   analogWrite(motorPin2, 0);
   analogWrite(motorPin3, speedB);
   analogWrite(motorPin4, 0);
@@ -62,7 +65,7 @@ void slightLeft()
 {
   analogWrite(motorPin1, 0);
   analogWrite(motorPin2, speedA);
-  analogWrite(motorPin3, speedB-45);
+  analogWrite(motorPin3, speedB-55);
   analogWrite(motorPin4, 0);
 }
 void slightMoreLeft()
@@ -84,7 +87,7 @@ void turnLeft()
   analogWrite(motorPin1, 0);
   analogWrite(motorPin2, speedA);
   analogWrite(motorPin3, 0);
-  analogWrite(motorPin4, speedB-65);
+  analogWrite(motorPin4, speedB);
 }
 
 
@@ -115,143 +118,113 @@ void dontMove()
 void readSensors()
 {
   
+  
   // Straight line
-  if (analogRead(midLeft)     <=500   &&    analogRead(nearLeft)    <=500   &&    analogRead(centerLeft)    >500  && 
-      analogRead(centerRight) >500    &&    analogRead(nearRight)   <=500   &&    analogRead(midRight)    <=500   )
+  /*======================================================================================================================================================*/
+  if (analogRead(farLeft)     <=hilo   &&    analogRead(midLeft)    <=hilo   &&    analogRead(nearLeft)    > hilo    && 
+      analogRead(nearRight)   > hilo   &&    analogRead(midRight)   <=hilo   &&    analogRead(farRight)    <=hilo    )
   {
     moveForward(); Serial.println("moveForward()");
   } else
+  /*======================================================================================================================================================*/
 
-
-
-  // Ajdust postion (move left)
-  /*==================================================================================================================================*/ /*
-  if (analogRead(farLeft)     <=500   &&    analogRead(midLeft)     <=500   &&    analogRead(nearLeft)    >500    ||    analogRead(centerLeft)    >500    && 
-      analogRead(centerRight) <=500   &&    analogRead(nearRight)   <=500   &&    analogRead(midRight)    <=500   &&    analogRead(farRight)      <=500   )
-  {
-    slightLeft();           Serial.println("slightLeft()");
-  } else
-  
-  if (analogRead(farLeft)     <=500   &&    analogRead(midLeft)     >500    ||    analogRead(nearLeft)    >500    &&    analogRead(centerLeft)    <=500   &&
-      analogRead(centerRight) <=500   &&    analogRead(nearRight)   <=500   &&    analogRead(midRight)    <=500   &&    analogRead(farRight)      <=500   )
-  {
-    slightMoreLeft();       Serial.println("slightMoreLeft()");
-  } else
-  
-  if (analogRead(farLeft)      >500   ||    analogRead(midLeft)     >500    &&    analogRead(nearLeft)    <=500   &&    analogRead(centerLeft)    <=500   &&
-      analogRead(centerRight) <=500   &&    analogRead(nearRight)   <=500   &&    analogRead(midRight)    <=500   &&    analogRead(farRight)      <=500   )
-  {
-    moreLeft();             Serial.println("moreLeft()"); 
-  } else
-  /*==================================================================================================================================*/
-  
-
-
-  // Adjust position (move right)
-  /*==================================================================================================================================*/ /*
-  if (analogRead(farLeft)     <=500   &&    analogRead(midLeft)     <=500   &&    analogRead(nearLeft)    <=500   &&    analogRead(centerLeft)    <=500   && 
-      analogRead(centerRight) >500    ||    analogRead(nearRight)   >500    &&    analogRead(midRight)    <=500   &&    analogRead(farRight)      <=500   )
-  {
-    slightRight();          Serial.println("slightRight()");
-  } else
-  
-  if (analogRead(farLeft)     <=500   &&    analogRead(midLeft)     <=500   &&    analogRead(nearLeft)    <=500   &&    analogRead(centerLeft)    <=500   && 
-      analogRead(centerRight) <=500   &&    analogRead(nearRight)   >500    ||    analogRead(midRight)    >500    &&    analogRead(farRight)      <=500   )
-  {
-    slightMoreRight();      Serial.println("slightMoreRight()");
-  } else
-  
-  if (analogRead(farLeft)     <=500   &&    analogRead(midLeft)     <=500   &&    analogRead(nearLeft)    <=500   &&    analogRead(centerLeft)    <=500   && 
-      analogRead(centerRight) <=500   &&    analogRead(nearRight)   <=500   &&    analogRead(midRight)    >500    ||    analogRead(farRight)      >500    )
-  {
-    moreRight();            Serial.println("moreRight()");
-  } else
-  /*==================================================================================================================================*/
 
 
 
   // Sharp turns
-  /*==================================================================================================================================*/
+  /*======================================================================================================================================================*/
   // Sharp left, Left turn
-  if (analogRead(midLeft)     >500    &&    analogRead(nearLeft)    >500    &&    analogRead(nearRight)   <=500   &&    analogRead(midRight)    <=500   )
+  if (analogRead(farLeft)     >hilo    &&    analogRead(midLeft)    >hilo    &&    analogRead(midRight)   <=hilo   &&    analogRead(farRight)    <=hilo   )
   {
     Serial.println("turningLeft()");
-    delay(2000);
-    turnLeft();             
+        analogWrite(motorPin1, 0);
+        analogWrite(motorPin2, speedA);
+        analogWrite(motorPin3, 0);
+        analogWrite(motorPin4, speedB-100);
     Serial.println("turnedLeft()");
   } else
 
   // Sharp right, Right turn
-  if (analogRead(midLeft)     <=500   &&    analogRead(nearLeft)    <=500   &&    analogRead(nearRight)   >500    &&    analogRead(midRight)    >500    )
+  if (analogRead(farLeft)     <=hilo   &&    analogRead(midLeft)    <=hilo   &&    analogRead(midRight)   >hilo    &&    analogRead(farRight)    >hilo    )
   {
     Serial.println("turningRight()");
-    delay(2000);
-    turnRight();            
-    Serial.println("turnRight()");
+        analogWrite(motorPin1, speedA-100);
+        analogWrite(motorPin2, 0);
+        analogWrite(motorPin3, speedB);
+        analogWrite(motorPin4, 0);
+    Serial.println("turnedRight()");
   } else
-  /*==================================================================================================================================*/
+  /*======================================================================================================================================================*/
 
 
 
+
+  //Intersection and Dead end
+  /*======================================================================================================================================================*/
   // T intersection
-  if (analogRead(midLeft)     >500    &&    analogRead(nearLeft)    >500    &&    analogRead(centerLeft)    >500    && 
-      analogRead(centerRight) >500    &&    analogRead(nearRight)   >500    &&    analogRead(midRight)    >500    )
+  if (analogRead(farLeft)     >hilo    &&    analogRead(midLeft)    >hilo    &&    analogRead(nearLeft)    >hilo    && 
+      analogRead(nearRight)   >hilo    &&    analogRead(midRight)   >hilo    &&    analogRead(farRight)    >hilo    )
   {
     turnLeft();             Serial.println("T intersect, turnLeft()");
   } else
   
   // Dead end
-  if (analogRead(midLeft)     <=500   &&    analogRead(nearLeft)    <=500   &&    analogRead(centerLeft)    <=500   && 
-      analogRead(centerRight) <=500   &&    analogRead(nearRight)   <=500   &&    analogRead(midRight)    <=500   )
+  if (analogRead(farLeft)     <=hilo   &&    analogRead(midLeft)    <=hilo   &&    analogRead(nearLeft)    <=hilo   && 
+      analogRead(nearRight)   <=hilo   &&    analogRead(midRight)   <=hilo   &&    analogRead(farRight)    <=hilo   )
   {
     turnAround();           Serial.println("turnAround()");
   }  else
+  /*======================================================================================================================================================*/
 
 
 
-  if (  analogRead(centerLeft)  >   50 + analogRead(centerRight) )
+
+  // Adjusting position
+  /*======================================================================================================================================================*/
+  if (  analogRead(nearLeft)  >   50 + analogRead(nearRight) )
   {
     slightLeft();           Serial.println("slightLeft()");
   } else
   
-  if (  analogRead(midRight)     <=500   &&    analogRead(nearRight)    <=500   &&    analogRead(centerRight)    <=500   &&    analogRead(centerLeft)    <=500 )
+  if (  analogRead(farRight)    <=hilo   &&   analogRead(midRight)    <=hilo   &&   analogRead(nearRight)   <=hilo   &&   analogRead(nearLeft)    <=hilo )
   {
-        if (  analogRead(nearLeft)    >   20 + analogRead(centerLeft) )
+        if (  analogRead(midLeft)   >   20 + analogRead(nearLeft) )
         {
           slightMoreLeft();           Serial.println("slightMoreLeft()");
         } else
-        if (  analogRead(midLeft)     >   20 + analogRead(nearLeft) )
+        if (  analogRead(farLeft)   >   20 + analogRead(midLeft) )
         {
           moreLeft();           Serial.println("MoreLeft()");
         } 
   } else
 
 
-  if (  50 + analogRead(centerLeft)  <   analogRead(centerRight) )
+  if (  50 + analogRead(nearLeft)  <   analogRead(nearRight) )
   {
     slightRight();          Serial.println("slightRight()");
   } else
   
-  if (  analogRead(midLeft)     <=500   &&    analogRead(nearLeft)    <=500   &&    analogRead(centerLeft)    <=500     &&    analogRead(centerRight)    <=500 )
+  if (  analogRead(farLeft)   <=hilo   &&    analogRead(midLeft)    <=hilo   &&    analogRead(nearLeft)   <=hilo    &&    analogRead(nearRight)   <=hilo )
   {
-        if (  20 + analogRead(centerRight) <   analogRead(nearRight)   )
+        if (  20 + analogRead(nearRight) <   analogRead(midRight)   )
         {
           slightMoreRight();          Serial.println("slightMoreRight()");
         } else
-        if (  20 + analogRead(nearRight)   <   analogRead(midRight)   )
+        if (  20 + analogRead(midRight)   <   analogRead(farRight)   )
         {
           moreRight();          Serial.println("MoreRight()");
         } 
   }
-  
+  /*======================================================================================================================================================*/
+
 }
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Testiiii");
+  Serial.println("Test");
   Serial.println();Serial.print(speedA);Serial.print("   ");Serial.print(speedB);
-  
+  //delay(4000);
 
   // Declaring output pins
   pinMode(motorPin1,    OUTPUT);
@@ -260,14 +233,14 @@ void setup()
   pinMode(motorPin4,    OUTPUT);
 
   // Declaring input pins
-  //pinMode(farLeft,      INPUT);
+  pinMode(farLeft,      INPUT);
   pinMode(midLeft,      INPUT);
   pinMode(nearLeft,     INPUT);
-  pinMode(centerLeft,   INPUT);
-  pinMode(centerRight,  INPUT);
+  //pinMode(centerLeft,   INPUT);
+  //pinMode(centerRight,  INPUT);
   pinMode(nearRight,    INPUT);
   pinMode(midRight,     INPUT);
-  //pinMode(farRight,     INPUT);
+  pinMode(farRight,     INPUT);
 
 }
 
